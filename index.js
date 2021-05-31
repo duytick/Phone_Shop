a11a6527c1709fb2e26a68e9d512045ade6efcd0
duytick <duylinhh47@gmail.com>
2021-05-31 22:44:52 +0700

    add top-sale

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-$(document).ready(function(){
-    //Owl-Carousel
-    $("#banner-area .owl-carousel").owlCarousel({
-        dots: true,
+$(document).ready(function () {
+  //Owl-Carousel
+  $("#banner-area .owl-carousel").owlCarousel({
+    dots: true,
+    items: 1,
+    loop: true,
+    // autoplay: true,
+    // autoplayTimeout: 3000,
+  });
+  //Top-sales owl-Carousel
+  $("#top-sale .owl-carousel").owlCarousel({
+    dots: false,
+    nav: true,
+    loop: true,
+    responsive: {
+      0: {
         items: 1,
-        loop: true,
-    });
-    //Top-sales owl-Carousel
-    $(".owl-carousel").owlCarousel({
-        dots: true,
-        nav: true,
-      loop: true,
-      responsive: {
-        0: {
-          items: 1,
-        },
-        600: {
-          items: 1,
-        },
-        1000: {
-          items:5 ,
-        },
       },
-    });
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
 
 
+  
 });
