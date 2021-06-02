6aea444df5f2c29cd6ee85b5ac1b1fe18dae7b98
duytick <duylinhh47@gmail.com>
2021-06-03 01:18:49 +0700

    #1: create footer part

--- index.js
+++ index.js
@@ -25,15 +25,47 @@ $(document).ready(function () {
     },
   });
 
-    //isotope filter
-    var $grid = $(".grid").isotope({
-      itemSelector: '.grid-item',
-      layoutMode: 'fitRows',
-    });
+  //isotope filter
+  var $grid = $(".grid").isotope({
+    itemSelector: ".grid-item",
+    layoutMode: "fitRows",
+  });
 
-    //filter items on button click
-    $(".button-group").on("click", "button", function () {
-      var filterValue = $(this).attr('data-filter');
-      $grid.isotope({ filter: filterValue });
-    });
+  //filter items on button click
+  $(".button-group").on("click", "button", function () {
+    var filterValue = $(this).attr("data-filter");
+    $grid.isotope({ filter: filterValue });
+  });
+
+  //New Phone owl-Carousel
+  $(" #new-phones .owl-carousel").owlCarousel({
+    dots: true,
+    nav: false,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+
+  $("#blogs .owl-carousel").owlCarousel({
+    dots: true,
+    nav: false,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+    },
+  });
 });
